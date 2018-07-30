87c439cbf73f16e0b8f524968dd1a89ca9191177
csj <chenshaojian@souche.com>
2018-07-30 14:03:56 +0800

    add new .py

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time      : 2018/7/30 下午1:53
+# @Author    : 陈少剑[chenshaojian@souche.com]
+# @CopyRight : DataTeam @ SouChe Inc
+# @Desc      :
+
+print 'hello word'
